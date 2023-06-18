fe53c9333c7fb9ff7c9de5ee69140ae82573ec24
Antônio Santana <antonionetosg6@gmail.com>
2023-06-18 17:25:13 -0300

    feat: implementa o header

--- src/components/CHeader.jsx
+++ src/components/CHeader.jsx
@@ -1,5 +1,47 @@
 export default function CHeader() {
   return (
-    <header>Header</header>
+    <header>
+      <a
+        href="/"
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2966/2966327.png"
+          alt="logo"
+          width="50px" />
+        <p>Bem Estar Distribuidora</p>
+      </a>
+      <input
+        placeholder="Pesquisa"
+      />
+      <a
+        href="/i/service"
+      >
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2598/2598911.png"
+          alt="atendimento"
+          width="35px"
+        />
+        <p>Atendimento</p>
+      </a>
+      <a
+      href="/u/account"
+      >
+        <img 
+        src="https://www.pngmart.com/files/21/Account-User-PNG-Photo.png"
+        alt="minha conta"
+        width="35px"
+        />
+        <p>Minha Conta</p>
+      </a>
+      <a
+      href="/u/cart"
+      >
+       <img 
+       src="https://www.freeiconspng.com/uploads/shopping-cart-icon-30.png"
+       alt="carrinho"
+       width="35px"
+       />
+      </a>
+    </header>
   );
 }
