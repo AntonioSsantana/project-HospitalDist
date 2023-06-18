9661a12a8b20874c38214a921ac71bc1b28cd6f7
Antônio Santana <antonionetosg6@gmail.com>
2023-06-18 12:58:32 -0300

    feat: criação da rota da página principal

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+// Pages
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/' element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
