ba7d62acfba29d33f236e71964822f166fe192da
Antônio Santana <antonionetosg6@gmail.com>
2023-06-19 01:47:56 -0300

    lint: corrige linter

--- src/mock/seringas.js
+++ src/mock/seringas.js
@@ -1,26 +1,26 @@
 const seringas = [
   {
-    "id": 21,
-    "name": "Seringa Descartável sem Agulha - BD",
-    "price": 0.62,
-    "company": "BD",
-    "type": "Seringa",
-    "image": "https://dentalpassaro.com.br/img/produtos/436/866/G/seringa-descartavel-sem-agulha-slip-bd-1.jpg",
-    "characteristics": null,
-    "description": `Confeccionadas em polipropileno especialmento desenvolvido para as seringas BD Plastipak™, 
+    'id': 21,
+    'name': 'Seringa Descartável sem Agulha - BD',
+    'price': 0.62,
+    'company': 'BD',
+    'type': 'Seringa',
+    'image': 'https://dentalpassaro.com.br/img/produtos/436/866/G/seringa-descartavel-sem-agulha-slip-bd-1.jpg',
+    'characteristics': null,
+    'description': `Confeccionadas em polipropileno especialmento desenvolvido para as seringas BD Plastipak™, 
     propocionando maior transparência`
   },
   {
-    "id": 27,
-    "name": "Seringa para Lavagem de Ouvido - Stylle",
-    "price": 352.20,
-    "company": "Stylle",
-    "type": "Seringa",
-    "image": "https://images.tcdn.com.br/img/img_prod/780820/seringa_para_lavagem_de_ouvido_100_cc_5647744_1_20200531135938.jpg",
-    "characteristics": null,
-    "description": `Confeccionadas em polipropileno especialmento desenvolvido para as seringas BD Plastipak™, 
+    'id': 27,
+    'name': 'Seringa para Lavagem de Ouvido - Stylle',
+    'price': 352.20,
+    'company': 'Stylle',
+    'type': 'Seringa',
+    'image': 'https://images.tcdn.com.br/img/img_prod/780820/seringa_para_lavagem_de_ouvido_100_cc_5647744_1_20200531135938.jpg',
+    'characteristics': null,
+    'description': `Confeccionadas em polipropileno especialmento desenvolvido para as seringas BD Plastipak™, 
     propocionando maior transparência`
   },
-]
+];
 
 export default seringas;
