51943765e24e0144f1de99b3093561ced9e6b6ba
Antônio Santana <antonionetosg6@gmail.com>
2023-06-19 01:46:49 -0300

    feat: implementa o footer

--- src/components/CFooter.jsx
+++ src/components/CFooter.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 
 export default function CFooter() {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <article>
+        <div>
+          <h4>Horário de atendimento</h4>
+          <p>Seg à Sex das 9h às 19h</p>
+          <p>Sab das 8h às 12h</p>
+        </div>
+        <div>
+          <h4>TELEFONE DE CONTATO</h4>
+          <p>{'(11) 94002-8922'}</p>
+        </div>
+      </article>
+      <article>
+        <div>
+          <h4>SAIBA MAIS</h4>
+          <h4>Redes Sociais</h4>
+        </div>
+      </article>
+      <article>
+        <p>COPYRIGHT © - Bem Estar Distribuidora</p>
+      </article>
+    </footer>
   );
 }
