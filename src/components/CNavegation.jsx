5e41ee27caeaedb59d36dd5c5def18a4b2b81312
Antônio Santana <antonionetosg6@gmail.com>
2023-06-19 00:10:22 -0300

    feat: adiciona categorias por tipo de produto

--- src/components/CNavegation.jsx
+++ src/components/CNavegation.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+
+// Mock
+import productsMock from '../mock';
 
 export default function CNavegation() {
+  const [productTypes, setProductsTypes] = useState([]);
+
+  useEffect(() => {
+    try {
+      if (productsMock) {
+        const uniqueTypes = [...new Set(productsMock.map((p) => p.type))];
+        setProductsTypes(uniqueTypes);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <nav>
       <div>
-        <span>VER CATEGORIAS</span>
+        {productTypes.map((t, index) => {
+          return (
+            <span
+              key={index}
+            >
+              {t}
+            </span>
+          );
+        })}
       </div>
     </nav>
   );
