c17639b89b943e72973529b2397af7b7c49b2fde
Antônio Santana <antonionetosg6@gmail.com>
2023-06-19 21:09:54 -0300

    feat: implementa botões e lógica para adição de produtos ao localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,40 @@ import {
 // Mock
 import productsMock from './mock';
 
-export default function App() {
+export default function Ap() {
 
-  const [products, setProducts] = useState([]);
+  const [products, setproducts] = useState([]);
   const [selectedType, setType] = useState('');
 
   useEffect(() => {
     try {
       if (productsMock) {
-        setProducts(productsMock);
+        setproducts(productsMock);
       }
     } catch (error) {
       console.error(error);
     }
   }, []);
+
+  const onClickFunction = ({ target }) => {
+    const { parentNode } = target;
+
+    try {
+      const productObject = {
+        name: parentNode.querySelector('p').textContent,
+        price: parentNode.querySelector('span').textContent,
+        image: parentNode.querySelector('img').getAttribute('src')
+      };
+
+      const cartItens = JSON.parse(localStorage.getItem('cart')) || [];
+
+      const newCart = Array.isArray(cartItens) ? [...cartItens, productObject] : [productObject];
+
+      localStorage.setItem('cart', JSON.stringify(newCart));
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div>
@@ -31,14 +51,23 @@ export default function App() {
         setType={setType}
       />
       <main>
-        {selectedType === 'Pressão' ? (
+        {selectedType === 'pressão' ? (
           productsMock
-            .filter((p) => p.type === 'Pressão')
+            .filter((p) => p.type === 'pressão')
             .map((p, index) => (
               <article key={index}>
-                <span>{p.name}</span>
+                <p>
+                  {p.name}
+                </p>
                 <img src={p.image} alt={p.name} width="50px" />
-                <span>{`R$ ${p.price.toFixed(2).toString().replace('.', ',')}`}</span>
+                <span>
+                  {`R$ ${p.price.toFixed(2).toString().replace('.', ',')}`}
+                </span>
+                <button
+                  onClick={onClickFunction}
+                >
+                  Adicionar
+                </button>
               </article>
             ))
         ) : selectedType === 'Equipo' ? (
@@ -46,9 +75,16 @@ export default function App() {
             .filter((p) => p.type === 'Equipo')
             .map((p, index) => (
               <article key={index}>
-                <span>{p.name}</span>
+                <p>
+                  {p.name}
+                </p>
                 <img src={p.image} alt={p.name} width="50px" />
                 <span>{`R$ ${p.price.toFixed(2).toString().replace('.', ',')}`}</span>
+                <button
+                  onClick={onClickFunction}
+                >
+                  Adicionar
+                </button>
               </article>
             ))
         ) : selectedType === 'Luvas' ? (
@@ -56,9 +92,16 @@ export default function App() {
             .filter((p) => p.type === 'Luvas')
             .map((p, index) => (
               <article key={index}>
-                <span>{p.name}</span>
+                <p>
+                  {p.name}
+                </p>
                 <img src={p.image} alt={p.name} width="50px" />
                 <span>{`R$ ${p.price.toFixed(2).toString().replace('.', ',')}`}</span>
+                <button
+                  onClick={onClickFunction}
+                >
+                  Adicionar
+                </button>
               </article>
             ))
         ) : selectedType === 'Seringa' ? (
@@ -66,16 +109,30 @@ export default function App() {
             .filter((p) => p.type === 'Seringa')
             .map((p, index) => (
               <article key={index}>
-                <span>{p.name}</span>
+                <p>
+                  {p.name}
+                </p>
                 <img src={p.image} alt={p.name} width="50px" />
                 <span>{`R$ ${p.price.toFixed(2).toString().replace('.', ',')}`}</span>
+                <button
+                  onClick={onClickFunction}
+                >
+                  Adicionar
+                </button>
               </article>
             ))
         ) : products.map((p, index) => (
           <article key={index}>
-            <span>{p.name}</span>
+            <p>
+              {p.name}
+            </p>
             <img src={p.image} alt={p.name} width="50px" />
             <span>{`R$ ${p.price.toFixed(2).toString().replace('.', ',')}`}</span>
+            <button
+              onClick={onClickFunction}
+            >
+              Adicionar
+            </button>
           </article>
         ))}
       </main>
