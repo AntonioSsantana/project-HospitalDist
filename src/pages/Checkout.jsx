0c1730a8e786cbd762b0c6abb3de6e92583319bd
Antônio Santana <antonionetosg6@gmail.com>
2023-06-21 15:37:45 -0300

    feat: implementação inicial dos elementos

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -2,6 +2,32 @@ import React from 'react';
 
 export default function Checkout() {
   return (
-    <h1>Checkout</h1>
+    <div>
+      <header>
+        <div>
+          <a
+            href="/"
+          >
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/2966/2966327.png"
+              alt="logo"
+              width="50px" />
+            <p>Bem Estar Distribuidora</p>
+          </a>
+        </div>
+      </header>
+      <main>
+        <div>
+          <h3>Carrinho</h3>
+        </div>
+        <div>
+          <p>Total: R$</p>
+        </div>
+        <div>
+          <button>ADICIONAR MAIS PRODUTOS</button>
+          <button>FECHAR PEDIDO</button>
+        </div>
+      </main>
+    </div>
   );
 }
