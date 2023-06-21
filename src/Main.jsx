76c2fe7419751f3729e42f45456e8507288ddb12
Antônio Santana <antonionetosg6@gmail.com>
2023-06-21 15:36:26 -0300

    feat: implementação da rota e criação da página

--- src/Main.jsx
+++ src/Main.jsx
@@ -4,12 +4,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 // Pages
 import App from './App.jsx';
+import {
+  Checkout
+} from './pages/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<App />} />
+        <Route path='/checkout/cart' element={<Checkout />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
