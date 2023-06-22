67574b9cb3fd0a009a76ddcbfd4281f3b3d3932c
Antônio Santana <antonionetosg6@gmail.com>
2023-06-22 13:06:45 -0300

    feat: corrige e implementa corretamente a função de adição e remoção de quantidade

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -3,33 +3,39 @@ import React, { useEffect, useState } from 'react';
 export default function Checkout() {
 
   const [checkoutProducts, setCheckoutProducts] = useState([]);
-  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     try {
       const products = JSON.parse(localStorage.getItem('cart'));
-      setCheckoutProducts(products);
+      setCheckoutProducts(products.map((product) => ({ ...product, quantity: 1 })));
     } catch (error) {
       console.error(error);
     }
   }, []);
 
   const onClickFunction = ({ target }) => {
-    const { name } = target;
+    const { parentNode, name } = target;
+    const grandParent = parentNode.parentNode;
+    const productName = grandParent.querySelector('span').textContent;
 
-    switch (name) {
-    case 'addiction-quantity':
-      setQuantity(quantity + 1);
-      break;
-    case 'reduce-quantity':
-      if (quantity === 1) {
-        break;
+    const updatedProducts = checkoutProducts.map((product) => {
+      if (product.name === productName) {
+        switch (name) {
+        case 'add':
+          return { ...product, quantity: product.quantity + 1 };
+        case 'remove':
+          if (product.quantity > 1) {
+            return { ...product, quantity: product.quantity - 1 };
+          }
+          break;
+        default:
+          break;
+        }
       }
-      setQuantity(quantity - 1);
-      break;
-    default:
-      break;
-    }
+      return product;
+    });
+
+    setCheckoutProducts(updatedProducts);
   };
 
   return (
@@ -72,21 +78,21 @@ export default function Checkout() {
                           alt={`${p.name} image`}
                           width='50px'
                         />
-                        {p.name}
+                        <span>{p.name}</span>
                       </td>
                       <td>
                         {p.price}
                       </td>
                       <td>
                         <button
-                          name='reduce-quantity'
+                          name='remove'
                           onClick={onClickFunction}
                         >
                           -
                         </button>
-                        <p>{quantity}</p>
+                        <p>{p.quantity}</p>
                         <button
-                          name='addiction-quantity'
+                          name='add'
                           onClick={onClickFunction}
                         >
                           +
@@ -94,8 +100,8 @@ export default function Checkout() {
                       </td>
                       <td>
                         {
-                          quantity > 1 ? (
-                            `R$ ${(p.price.replace('R$', '').replace(',', '.') * quantity).toFixed(2).replace('.', ',')}`
+                          p.quantity > 1 ? (
+                            `R$ ${(p.price.replace('R$', '').replace(',', '.') * p.quantity).toFixed(2).replace('.', ',')}`
                           ) : (p.price)
                         }
                       </td>
@@ -106,7 +112,7 @@ export default function Checkout() {
             </tbody>
           </table>
         </div>
-      </main>
-    </div>
+      </main >
+    </div >
   );
 }
