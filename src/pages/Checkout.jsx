887e274c4878d7701520698c4165604c955640e5
Antônio Santana <antonionetosg6@gmail.com>
2023-06-22 11:52:28 -0300

    feat: adiciona produtos na pagina de carrinho e implementa estado

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Checkout() {
+
+  const [checkoutProducts, setCheckoutProducts] = useState([]);
+
+  useEffect(() => {
+    try {
+      const products = JSON.parse(localStorage.getItem('cart'));
+      setCheckoutProducts(products);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <div>
       <header>
@@ -19,13 +31,39 @@ export default function Checkout() {
       <main>
         <div>
           <h3>Carrinho</h3>
-        </div>
-        <div>
-          <p>Total: R$</p>
-        </div>
-        <div>
-          <button>ADICIONAR MAIS PRODUTOS</button>
-          <button>FECHAR PEDIDO</button>
+          <table>
+            <thead>
+              <tr>
+                <th>Produto</th>
+                <th>Preço</th>
+                <th>Quantidade</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                checkoutProducts.map((p, index) => {
+                  return (
+                    <tr
+                      key={index}
+                    >
+                      <td>
+                        <img
+                          src={p.image}
+                          alt={`${p.name} image`}
+                          width='50px'
+                        />
+                        {p.name}
+                      </td>
+                      <td>
+                        {p.price}
+                      </td>
+                    </tr>
+                  );
+                })
+              }
+            </tbody>
+          </table>
         </div>
       </main>
     </div>
