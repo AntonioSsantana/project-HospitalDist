df3d9518f7750e263d18e8adbd99a57460bf57a8
Antônio Santana <antonionetosg6@gmail.com>
2023-06-23 17:34:58 -0300

    feat: implementação inicial dos elementos

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -2,6 +2,52 @@ import React from 'react';
 
 export default function AccountPage() {
   return (
-    <h1>Account Page</h1>
+    <form>
+      <label
+        htmlFor='username'
+      >
+        Nome de usuário
+      </label>
+      <input
+        type='text'
+        id='username'
+      />
+      <label
+        htmlFor='email'
+      >
+        Email
+      </label>
+      <input
+        type='text'
+        id='email'
+      />
+      <label
+        htmlFor='password'
+      >
+        Senha
+      </label>
+      <input
+        type='password'
+        id='password'
+      />
+      <label
+        htmlFor='cpf-input'
+      >
+        CPF
+      </label>
+      <input
+        type='text'
+        id='cpf-input'
+      />
+      <label
+        htmlFor='number'
+      >
+        Número
+      </label>
+      <input
+        type='tel'
+        id='number'
+      />
+    </form>
   );
 }
