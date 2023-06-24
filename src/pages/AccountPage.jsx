0b4f9e2429196c0409f9ee5eb3190e7188a4e3c7
Antônio Santana <antonionetosg6@gmail.com>
2023-06-24 16:23:05 -0300

    feat: adiciona estados e habilitar botão ao validar cpf

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,53 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CPFvalidation from '../validations/CPFvalidation';
 
 export default function AccountPage() {
+
+  const [cpfValue, setCpfValue] = useState('');
+
+  const [isDisable, setEnableButton] = useState(true);
+
+  useEffect(() => {
+    if (CPFvalidation(cpfValue)) {
+      setEnableButton(false);
+    } else {
+      setEnableButton(true);
+    }
+  }, [cpfValue]);
+
+  const onChangeFunction = ({ target }) => {
+    const { name, value } = target;
+
+    switch (name) {
+    case 'cpf-input':
+      setCpfValue(value);
+      break;
+    default:
+      break;
+    }
+  };
+
   return (
     <form>
-      <label
-        htmlFor='username'
+      <div>
+        <label
+          htmlFor='username'
+        >
+          Nome de usuário *
+        </label>
+        <input
+          type='text'
+          id='username'
+        />
+      </div>
+      <div>
+        <label
+          htmlFor='email'
+        >
+          Email *
+        </label>
+        <input
+          type='text'
+          id='email'
+        />
+      </div>
+      <div>
+        <label
+          htmlFor='password'
+        >
+          Senha *
+        </label>
+        <input
+          type='password'
+          id='password'
+        />
+      </div>
+      <div>
+        <label
+          htmlFor='cpf-input'
+        >
+          CPF *
+        </label>
+        <input
+          type='text'
+          id='cpf-input'
+          name="cpf-input"
+          onChange={onChangeFunction}
+        />
+      </div>
+      <div>
+        <label
+          htmlFor='number'
+        >
+          Número *
+        </label>
+        <input
+          type='tel'
+          id='number'
+        />
+      </div>
+      <button
+        disabled={isDisable}
       >
-        Nome de usuário
-      </label>
-      <input
-        type='text'
-        id='username'
-      />
-      <label
-        htmlFor='email'
-      >
-        Email
-      </label>
-      <input
-        type='text'
-        id='email'
-      />
-      <label
-        htmlFor='password'
-      >
-        Senha
-      </label>
-      <input
-        type='password'
-        id='password'
-      />
-      <label
-        htmlFor='cpf-input'
-      >
-        CPF
-      </label>
-      <input
-        type='text'
-        id='cpf-input'
-      />
-      <label
-        htmlFor='number'
-      >
-        Número
-      </label>
-      <input
-        type='tel'
-        id='number'
-      />
+        Confirmar
+      </button>
     </form>
   );
 }
